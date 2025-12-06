45035f282e0c1927b24ed2e9814a67e782ab513a
John Herrlin <jherrlin@gmail.com>
2025-12-06 18:19:00 +0100

    clojure day3 prolem 2

---
+++ clojure/day3/problem2.clj
@@ -0,0 +1,51 @@
+(ns user
+  (:require [clojure.string :as str]))
+
+
+(defn input []
+  (->> "input.txt"
+       slurp
+       str/split-lines
+       (map str/trim)))
+
+(def test-input
+  (->> "987654321111111
+811111111111119
+234234234234278
+818181911112111"
+       str/split-lines
+       (map str/trim)))
+
+(defn parse-bank [line]
+  (mapv #(Integer/parseInt (str %)) line))
+
+(defn solve [acc nb bank]
+  (cond
+    (zero? nb)
+    acc
+
+    (= nb (count bank))
+    (concat acc bank)
+
+    :else
+    (let [split-index     (- (count bank) (dec nb))
+          candidates      (take split-index bank)
+          [next-idx max]  (->> (map-indexed vector candidates)
+                               (sort-by second #(compare %2 %1))
+                               first)
+          remaining-banks (drop (inc next-idx) bank)]
+      (recur (conj acc max) (dec nb) remaining-banks))))
+
+(defn calc [xs]
+  (->> xs
+       (mapv parse-bank)
+       (mapv #(solve [] 12 %))
+       (map (fn [digits] (Long/parseLong (apply str digits))))
+       (reduce +)))
+
+
+(calc test-input)
+;; => 3121910778619
+
+(calc (input))
+;; => 171741365473332
